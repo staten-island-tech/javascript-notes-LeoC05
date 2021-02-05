27977357c308b6d8326c33e36ec1de80020ee1d0
LeoC05 <71610558+LeoC05@users.noreply.github.com>
2021-02-04 21:27:52 -0500

    ok

---
+++ app.js
@@ -0,0 +1,39 @@
+/*var life = 100;
+
+var name = 'Leo';
+
+life = life - 10; //90 life
+
+var checkout = false; //BOOLEAN
+
+var box; */ //Old method
+
+//const life = 100; //Const means once you identify it, it will stay constant
+
+let life = 100;
+
+life = life - 50;
+
+console.log(life); //50
+
+const myAge = 24;
+
+const yourAge = 21;
+
+console.log(myAge + yourAge); //45
+
+console.log("Hello my name is " + "Ed");
+
+const name = "Edwin";
+
+console.log(`Hello it's me and my name is ${name}`);// ` is left of "1" key NEW METHOD
+
+const age = 24;
+
+//console.log("Hello it's me " + name + " and my age is " + age + " and I'm getting old");// old method
+
+const combined = name + age;
+
+console.log(combined); //Edwin24
+
+console.log(typeof age);//number
