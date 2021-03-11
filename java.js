044ffba8faaa8de5777aa72d9be6faf727f8f296
LeoC05 <71610558+LeoC05@users.noreply.github.com>
2021-03-10 22:40:44 -0500

    a

--- java.js
+++ java.js
@@ -1,52 +1,73 @@
-//document
-document.getElementById('page-banner'); //gets element with id page-banenr
-var banner = document.getElementById('page-banner'); //stores element into variable
-document.getElementsByClassName('title'); //gets elements with the class of title
-var titles = document.getElementsByClassName('title'); //able to retrieve indexes like an array
-var lis = document.getElementsByTagName('li'); //gets all elements of li
-
-for(i=0; i < titles.length; i++) { //cycles through html collection
-    console.log(titles[i]);
-}
-
-titles.forEach(function(item) {
-    console.log(item);
-}) //errors because this is not an array but an html collection
-
-console.log(Array.isArray(titles)); //passes true if array, false if not
-console.log(Array.isArray(Array.from(titles)));//Creates an array from titles html collection
-
-Array.from(titles).forEach(function(item) {
-    console.log(item);
-}) //works because turned into array
-
-
-$('#wrapper');//grabs wrapper element
-document.querySelector('#wrapper'); //grabs wrapper element
-const wrap = document.querySelector('#wrapper'); //saves it to variable
-console.log(wrap);
-
-const wmf = document.querySelector('#book-list li:nth-child(2) .name')//grabs li in book-list 2nd child with class of name
-console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-console.log(books);// only grabs one because only grabs first
-
-books = document.querySelectorAll('#book-list li .name'); //this grabs all now
-
-Array.from(books).forEach(function(book) { //turns node list into an array and cycles through it
-    console.log(book);
-})
-
-
-
-
-Array.from(books).forEach(function(book) {
-    //book.textContent = 'test'; //changes text content to test
-    book.textContent += ' (book title)'; //append/add text content to test
-})
-
 const bookList = document.querySelector('#book-list');
-//console.log(bookList.innerHTML);//logs all html inside #book-list
-bookList.innerHTML = '<h2>Books and more books...</h2>'; //replaces content with the new h2
-bookList.innerHTML += '<p>This is how you add HTML</p>';//appends/adds to the innerHTML
+
+console.log('the parent node is:', bookList.parentNode); 
+console.log('the parent element is:', bookList.parentElement.parentElement);
+
+console.log(bookList.children);
+
+
+console.log('book-list next sibling is:', bookList.nextSibling);
+console.log('book-list next element sibling is:', bookList.nextElementSibling);
+console.log('book-list previous sibling is:', bookList.previousSibling);
+console.log('book-list previous element sibling is:', bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
+
+var h2 = document.querySelector('#book-list h2');
+
+h2.addEventListener('click', function(e) { //listens for click event on h2
+    console.log(e.target); //tells target of event
+    consolge.log(e);
+});
+
+var btns = document.querySelectorAll('#book-list .delete')//references all delete buttons
+
+Array.from(btns).forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li) //li's parent remove child
+    });
+});
+
+const link = document.querySelector('#page-banner a');
+link.addEventListener('click', function(e) { //prevents link from default action whic his navigating to other website
+    e.preventDefault();
+    console.log('navigation to', e.target.textContent, 'was prevented');
+});
+
+const list = document.querySelector('#book-list ul');
+
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li);
+    }
+});
+
+
+//add book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e) { //submit is added to the form, not the button
+    e.preventDefault();//normally would refresh the page, now prevented
+    const value = addForm.querySelector('input[type="text"]').value
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    //add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    //append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
+
+
